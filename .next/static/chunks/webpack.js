861fa9653b93f0ffd612d4091c0e91d93e6e62c6
Steph Shum <stephshum@Stephs-MacBook-Pro.local>
2025-09-30 19:19:52 -0700

    Add debugging to ProductPage to track checkout flow

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "08eba1a8753281b5"; }
+/******/ 		__webpack_require__.h = function() { return "5ca89160b3f8c366"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
